87a53a204aec4e08c6ddb1c3919181386b9b4a04
sunny(sAM) <101540270+sunnykumarsam@users.noreply.github.com>
2022-04-05 19:28:54 +0530

    lower type.java
    5-apr-2022

---
+++ lowertype.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Adding lowertype
+Sunnykumark
+5-apr-2022
+*******************************************************************************/
+
+public class Main{
+    public static void main(String []args){
+        byte a=5;
+        byte b=7;
+        byte c=(byte)(a+b);
+        System.out.println("the sum of a and b:- "+c);
+        
+    }
+}
